7d2f4e35df8f27f4c653ded818e87f82b86ef76e
takoyjr <niktatakoy2607@gmail.com>
2023-12-29 19:27:48 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Converter } from './component/Converter'
+
+function App() {
+  return (
+    <div className="h-screen flex w-full justify-center items-center">
+      <Converter />
+    </div>
+  );
+}
+
+export default App;
